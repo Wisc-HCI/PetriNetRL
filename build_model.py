7efef7a91c259660456d785c99442f44fd1052b7
Nathan White <ntwhite@wisc.edu>
2024-09-30 08:43:30 -0500

    Rename to build_model, adding argparse

---
+++ build_model.py
@@ -0,0 +1,117 @@
+import gymnasium as gym
+import numpy as np
+
+from sb3_contrib.common.maskable.policies import MaskableActorCriticPolicy
+from sb3_contrib.common.wrappers import ActionMasker
+from sb3_contrib.ppo_mask import MaskablePPO
+from stable_baselines3 import PPO
+import os
+from petrienv import PetriEnv
+from deadlockenv import DeadlockEnv
+from explorationenv import ExplorationEnv
+import time
+import json
+from datetime import datetime
+from constants import *
+import argparse
+
+# Rectify the numpy versions
+import os
+os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
+
+def run(filename):
+    start = datetime.now()
+
+    def mask_fn(env: gym.Env) -> np.ndarray:
+        # Do whatever you'd like in this function to return the action mask
+        # for the current env. In this example, we assume the env has a
+        # helpful method we can rely on.
+        return env.valid_action_mask()
+
+    if not os.path.exists(models_dir):
+        os.makedirs(models_dir)
+
+    if not os.path.exists(logdir):
+        os.makedirs(logdir)
+
+    # with open('test_run.json', encoding='utf-8') as fh:
+    f = FILENAME
+    if filename is not None:
+        f = filename
+    with open(f, encoding='utf-8') as fh:
+        json_obj = json.load(fh)
+
+    firstEnv = DeadlockEnv(json_obj)
+    secondEnv = ExplorationEnv(json_obj)
+    thirdEnv = PetriEnv(json_obj)
+
+
+    firstEnv.reset(0, {})
+    secondEnv.reset(0, {})
+    firstEnv = ActionMasker(firstEnv, mask_fn)  # Wrap to enable masking
+    secondEnv = ActionMasker(secondEnv, mask_fn)  # Wrap to enable masking
+    thirdEnv = ActionMasker(thirdEnv, mask_fn)  # Wrap to enable masking
+
+    # Train on the deadlock environment first
+    model = MaskablePPO(MaskableActorCriticPolicy, firstEnv, verbose=1, tensorboard_log=logdir, device="auto")
+
+    if DEADLOCK_TRAINING:
+        iters = 0
+        while iters < MAX_DEADLOCK_ITERATIONS:
+            iters += 1
+            model.learn(total_timesteps=DEADLOCK_TIMESTEPS)
+            if iters % DEADLOCK_ITERATION_SAVE_INTERVAL == 0:
+                model.save(f"{models_dir}/Deadlock-{iters}")
+        # After training, save and load the model to change environments for the next round of training
+        model.save(f"{models_dir}/Deadlock-final")
+        if EXPLORATION_TRAINING:
+            model = MaskablePPO.load(f"{models_dir}/Deadlock-final", secondEnv, verbose=1, tensorboard_log=logdir, device="auto")
+        else:
+            model = MaskablePPO.load(f"{models_dir}/Deadlock-final", thirdEnv, verbose=1, tensorboard_log=logdir, device="auto")
+    else:
+        model = MaskablePPO.load(f"{models_dir}/Deadlock-5", secondEnv, verbose=1, tensorboard_log=logdir, device="auto")
+
+        # model = MaskablePPO(MaskableActorCriticPolicy, secondEnv, verbose=1, tensorboard_log=logdir, device="auto")
+        # model = PPO('MlpPolicy', secondEnv, verbose=1, tensorboard_log=logdir)
+
+    deadlock_time = datetime.now()
+
+    if EXPLORATION_TRAINING:
+        iters = 0
+        while iters < MAX_EXPLORATION_ITERATIONS:
+            iters += 1
+            model.learn(total_timesteps=EXPLORATION_TIMESTEPS)
+            if iters % EXPLORATION_ITERATION_SAVE_INTERVAL == 0:
+                model.save(f"{models_dir}/Exploration-{iters}")
+        # After training, save and load the model to change environments for the next round of training
+        model.save(f"{models_dir}/Exploration-final")
+        model = MaskablePPO.load(f"{models_dir}/Exploration-final", thirdEnv, verbose=1, tensorboard_log=logdir, device="auto")
+    elif not DEADLOCK_TRAINING:
+        model = MaskablePPO.load(f"{models_dir}/Deadlock-10", thirdEnv, verbose=1, tensorboard_log=logdir, device="auto")
+
+    exploration_time = datetime.now()
+
+    if PPO_TRANING:
+        # Train on the actual environment after we've learned to avoid deadlock scenarios
+        iters = 0
+        while iters < MAX_PPO_ITERATIONS:
+            iters += 1
+            model.learn(total_timesteps=PPO_TIMESTEPS)
+            if iters % PPO_ITERATION_SAVE_INTERVAL == 0:
+                model.save(f"{models_dir}/PPO-{iters}")
+        model.save(f"{models_dir}/PPO-final")
+
+    ppo_time = datetime.now()
+
+    print("Deadlock TIME: {0}".format(deadlock_time - start))
+    print("Exploration TIME: {0}".format(exploration_time - deadlock_time))
+    print("PPO TIME: {0}".format(ppo_time - exploration_time))
+    print("TOTAL TIME: {0}".format(datetime.now() - start))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--file", type=str, default=None, help="")
+    args = parser.parse_args()
+
+    run(args.file)
--- test_model.py
+++ test_model.py
@@ -12,6 +12,7 @@ import time
 import json
 from constants import *
 import csv
+import argparse
 
 def is_sim_type(transition):
     for obj in transition["metaData"]:
@@ -42,121 +43,135 @@ def mask_fn(env: gym.Env) -> np.ndarray:
     # helpful method we can rely on.
     return env.valid_action_mask()
 
-# Load initial network from json
-with open(FILENAME, encoding='utf-8') as fh:
-    json_obj = json.load(fh)
 
-# Setup model and environment
-env = PetriEnv(json_obj)
-# env = DeadlockEnv(json_obj)
-# env = ExplorationEnv(json_obj)
+def run(filename):
+    f = FILENAME
+    if filename is not None:
+        f = filename
+    # Load initial network from json
+    with open(f, encoding='utf-8') as fh:
+        json_obj = json.load(fh)
 
-# Mask
-env.reset(0, {})
-env = ActionMasker(env, mask_fn)  # Wrap to enable masking
-model = MaskablePPO.load("models/Deadlock-PPO/Exploration-30.zip")
-# model = MaskablePPO.load("models/PPO/PPO-50.zip")
-obs, info = env.reset()
+    # Setup model and environment
+    # env = PetriEnv(json_obj)
+    # env = DeadlockEnv(json_obj)
+    env = ExplorationEnv(json_obj)
 
-# Non-Mask
-# env.reset(0)
-# model = PPO.load("models/1696260194/PPO-1.zip")
-# obs, info = env.reset(0)
-
-done = False
-
-def is_goal(marking, goal_state):
-    places = np.where(goal_state == 1)[0]
-    for i in places:
-        if marking[i][0] < 1:
-            return False
-    return True
-
-print("intial observation:")
-for i, row in enumerate(obs):
-    print(env.place_names[i], "\t", row[0])
-print('---------')
-
-cummulative_reward = 0.0
-iteration = 0
-action_sequence = []
-while not done:
-    iteration += 1
     # Mask
-    action, _states = model.predict(obs, action_masks=mask_fn(env))
+    env.reset(0, {})
+    env = ActionMasker(env, mask_fn)  # Wrap to enable masking
+    model = MaskablePPO.load("models/Deadlock-PPO/Exploration-final.zip")
+    # model = MaskablePPO.load("models/PPO/PPO-50.zip")
+    obs, info = env.reset()
+
     # Non-Mask
-    # action, _states = model.predict(obs)
-    obs, rewards, done, shortcut, info = env.step(action)
-    cummulative_reward += rewards
-    # print("transition:", env.transition_names[action])
-    action_sequence.append((env.transition_ids[action], action))
-    # print("reward", rewards)
-    # print("cumulative reward", cummulative_reward)
-    # print("resulting observation:")
-    # for i, row in enumerate(obs):
-    #     print(env.place_names[i], "\t", row[0])
-    # print('---------')
-    if iteration >= MAX_TESTING_TIMESTEPS or done:
-        done = True
-        # print(action_sequence)
-        print("reward", rewards)
-        print("cumulative reward", cummulative_reward)
+    # env.reset(0)
+    # model = PPO.load("models/1696260194/PPO-1.zip")
+    # obs, info = env.reset(0)
+
+    done = False
+
+    def is_goal(marking, goal_state):
+        places = np.where(goal_state == 1)[0]
+        for i in places:
+            if marking[i][0] < 1:
+                return False
+        return True
+
+    print("intial observation:")
+    for i, row in enumerate(obs):
+        print(env.place_names[i], "\t", row[0])
+    print('---------')
+
+    cummulative_reward = 0.0
+    iteration = 0
+    action_sequence = []
+    while not done:
+        iteration += 1
+        # Mask
+        action, _states = model.predict(obs, action_masks=mask_fn(env))
+        # Non-Mask
+        # action, _states = model.predict(obs)
+        obs, rewards, done, shortcut, info = env.step(action)
+        cummulative_reward += rewards
+        # print("transition:", env.transition_names[action])
+        action_sequence.append((env.transition_ids[action], action))
+        # print("reward", rewards)
+        # print("cumulative reward", cummulative_reward)
         # print("resulting observation:")
         # for i, row in enumerate(obs):
         #     print(env.place_names[i], "\t", row[0])
-        print("is goal met? + {0}".format(is_goal(obs, env.goal_state)))
-        print("Ending due to iteration cap or done flag")
-        print("iteration", iteration, "Max", MAX_TESTING_TIMESTEPS)
-    #     print("cumulative reward", cummulative_reward)
+        # print('---------')
+        if iteration >= MAX_TESTING_TIMESTEPS or done:
+            done = True
+            # print(action_sequence)
+            print("reward", rewards)
+            print("cumulative reward", cummulative_reward)
+            # print("resulting observation:")
+            # for i, row in enumerate(obs):
+            #     print(env.place_names[i], "\t", row[0])
+            print("is goal met? + {0}".format(is_goal(obs, env.goal_state)))
+            print("Ending due to iteration cap or done flag")
+            print("iteration", iteration, "Max", MAX_TESTING_TIMESTEPS)
+        #     print("cumulative reward", cummulative_reward)
 
 
-currentTime = 0
-with open(OUTPUT, "w+", newline='') as fh:
-    csv_writer = csv.writer(fh)
-    csv_writer.writerow(["Action", "Type", "Agent Assigned To Task", "From Standing Location", "To Standing Location", "From Hand Location", "To Hand Location", "Start Time (s)", "End Time (s)", "Hand Cost", "Arm Cost", "Shoulder Cost", "Whole Body Cost", "Monetary Cost", "Primitives", "MVC", "Hand Distance", "Stand Distance", "Is One Handed"])
+    currentTime = 0
+    with open(OUTPUT, "w+", newline='') as fh:
+        csv_writer = csv.writer(fh)
+        csv_writer.writerow(["Action", "Type", "Agent Assigned To Task", "From Standing Location", "To Standing Location", "From Hand Location", "To Hand Location", "Start Time (s)", "End Time (s)", "Hand Cost", "Arm Cost", "Shoulder Cost", "Whole Body Cost", "Monetary Cost", "Primitives", "MVC", "Hand Distance", "Stand Distance", "Is One Handed"])
 
-    for (transition_id, action) in action_sequence:
-        transition = json_obj["transitions"][transition_id]
-        costs = find_costs(transition)
-        duration = transition["time"]
-        primtives = ""
-        mvcs = ""
-        isOneHanded = ""
-        standDistanceTraveled = ""
-        handDistanceTraveled = ""
-        a = 0
-        fromHandLocation = ""
-        toHandLocation = ""
-        fromStandLocation = ""
-        toStandLocation = ""
-        for m in transition["metaData"]:
-            if m["type"] == "primitiveAssignment":
-                try:
-                    primtives += json_obj["nameLookup"][m["value"][1]] + ";"
-                except:
-                    a = 1
-            elif m["type"] == "isOneHanded":
-                isOneHanded += "True" if m["value"][1] else "False" + ";"
-            elif m["type"] == "mVC":
-                mvcs += str(m["value"][1]) + ";"
-            elif m["type"] == "handTravelDistance":
-                handDistanceTraveled += str(m["value"][1]) + ";"
-            elif m["type"] == "standTravelDistance":
-                standDistanceTraveled += str(m["value"][1]) + ";"
-            elif m["type"] == "standing":
-                fromStandLocation = json_obj["nameLookup"][m["value"][0]]
-                toStandLocation = fromStandLocation
-            elif m["type"] == "hand":
-                fromHandLocation = json_obj["nameLookup"][m["value"][0]]
-                toHandLocation = fromHandLocation
-            elif m["type"] == "fromHandPOI":
-                fromHandLocation = json_obj["nameLookup"][m["value"][0]]
-            elif m["type"] == "toHandPOI":
-                toHandLocation = json_obj["nameLookup"][m["value"][0]]
-            elif m["type"] == "fromStandingPOI":
-                fromStandLocation = json_obj["nameLookup"][m["value"][0]]
-            elif m["type"] == "toStandingPOI":
-                toStandLocation = json_obj["nameLookup"][m["value"][0]]
-        # TODO: keep track of time, busy/working agents, who all is assigned to a task (multiple agent split)
-        csv_writer.writerow([env.transition_names[action], is_sim_type(transition), "", fromStandLocation, toStandLocation, fromHandLocation, toHandLocation, currentTime, currentTime+duration, costs[0], costs[1], costs[2], costs[3], costs[4], primtives, mvcs, handDistanceTraveled, standDistanceTraveled, isOneHanded])
-        currentTime += duration
+        for (transition_id, action) in action_sequence:
+            transition = json_obj["transitions"][transition_id]
+            costs = find_costs(transition)
+            duration = transition["time"]
+            primtives = ""
+            mvcs = ""
+            isOneHanded = ""
+            standDistanceTraveled = ""
+            handDistanceTraveled = ""
+            a = 0
+            fromHandLocation = ""
+            toHandLocation = ""
+            fromStandLocation = ""
+            toStandLocation = ""
+            for m in transition["metaData"]:
+                if m["type"] == "primitiveAssignment":
+                    try:
+                        primtives += json_obj["nameLookup"][m["value"][1]] + ";"
+                    except:
+                        a = 1
+                elif m["type"] == "isOneHanded":
+                    isOneHanded += "True" if m["value"][1] else "False" + ";"
+                elif m["type"] == "mVC":
+                    mvcs += str(m["value"][1]) + ";"
+                elif m["type"] == "handTravelDistance":
+                    handDistanceTraveled += str(m["value"][1]) + ";"
+                elif m["type"] == "standTravelDistance":
+                    standDistanceTraveled += str(m["value"][1]) + ";"
+                elif m["type"] == "standing":
+                    fromStandLocation = json_obj["nameLookup"][m["value"][0]]
+                    toStandLocation = fromStandLocation
+                elif m["type"] == "hand":
+                    fromHandLocation = json_obj["nameLookup"][m["value"][0]]
+                    toHandLocation = fromHandLocation
+                elif m["type"] == "fromHandPOI":
+                    fromHandLocation = json_obj["nameLookup"][m["value"][0]]
+                elif m["type"] == "toHandPOI":
+                    toHandLocation = json_obj["nameLookup"][m["value"][0]]
+                elif m["type"] == "fromStandingPOI":
+                    fromStandLocation = json_obj["nameLookup"][m["value"][0]]
+                elif m["type"] == "toStandingPOI":
+                    toStandLocation = json_obj["nameLookup"][m["value"][0]]
+            # TODO: keep track of time, busy/working agents, who all is assigned to a task (multiple agent split)
+            csv_writer.writerow([env.transition_names[action], is_sim_type(transition), "", fromStandLocation, toStandLocation, fromHandLocation, toHandLocation, currentTime, currentTime+duration, costs[0], costs[1], costs[2], costs[3], costs[4], primtives, mvcs, handDistanceTraveled, standDistanceTraveled, isOneHanded])
+            currentTime += duration
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--file", type=str, default=None, help="")
+    args = parser.parse_args()
+
+    run(args.file)
